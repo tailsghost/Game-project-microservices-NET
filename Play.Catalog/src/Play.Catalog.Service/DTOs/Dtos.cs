9b735cca54a68d8aa84e666c6adf29f1fd62bdab
AndreyXP <andreyapple99@mail.ru>
2024-05-09 14:47:38 +0300

    Добавьте файлы проекта.

---
+++ Play.Catalog/src/Play.Catalog.Service/DTOs/Dtos.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Play.Catalog.Service.DTOs;
+
+public record ItemDto(Guid id, string Name, string Description, decimal Price, DateTimeOffset CreateDate);
+
+public record CreateItemDto(string Name, string Description, decimal Price);
+
+public record UpdateItemDto(string Name, string Description, decimal Price);
